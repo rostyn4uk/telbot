f7f489a8cd3d23ba4e2acd484a7c325cc3b25048
rostyn4uk <rostik.manager@gmail.com>
2020-05-16 13:23:21 +0300

    new2 commit1

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-// подключаем фреймворк
-const TelegramBot = require('node-telegram-bot-api');
-
-// здесь указывается токен, который получен от родительского бота выше
-const token = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
-// создаём бота
-const bot = new TelegramBot(token, { polling: true });
-
-    myName = console.log('Hello! My name is %s!', me.first_name);
-    myId = console.log('My id is %s.', me.id);
-    myUsername = console.log('And my username is @%s.', me.username);
-
-// проверяем чат и если введена команда "/start", выводим сообщение
-bot.onText(/\/start/, (msg, match) => {
-    // Составляем сообщение, которое будет содержать имя и фамилию того,
-    // кто взаимодействует с ботом
-    var message = 'Привет, ' + msg.chat.first_name;
-
-    // отсылаем сообщение, первым параметром передавая id чата,
-    // а вторым уже само сообщение
-    bot.sendMessage(msg.chat.id, message);
-}); 
+var TelegramBot = require('node-telegram-bot-api');
+ 
+var token = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
+var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
+ 
+bot.on('text', function(msg)
+{
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === '/say') {
+        sendMessageByBot(messageChatId, 'Hello World!');
+    }
+ 
+    console.log(msg);
+});
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
