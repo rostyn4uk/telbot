dfc0e1286841560dcdf1c7c8f4c15f7f3fa1fd55
rostyn4uk <rostik.manager@gmail.com>
2020-05-16 12:15:57 +0300

    Commit description

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
 const bot = new TelegramBot(TOKEN, {polling : true});
 bot.on('message' , msg => {
-    bot.sendMessage(msg.chat.id, 'Hello my Friend, bot says : Hi, ', msg.from.first_name);
+    bot.sendMessage(msg.chat.id, 'Hello my Friend, bot says : Hi, ', msg.from.first_name, 'Hello');
 });
+
