fc51292833783757bb940178107a840ca3acc0a9
rostyn4uk <rostik.manager@gmail.com>
2020-05-16 13:14:52 +0300

    new2 commit2

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const token = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
 // создаём бота
 const bot = new TelegramBot(token, { polling: true });
 
+    myName = console.log('Hello! My name is %s!', me.first_name);
+    myId = console.log('My id is %s.', me.id);
+    myUsername = console.log('And my username is @%s.', me.username);
+
 // проверяем чат и если введена команда "/start", выводим сообщение
 bot.onText(/\/start/, (msg, match) => {
     // Составляем сообщение, которое будет содержать имя и фамилию того,
     // кто взаимодействует с ботом
-    var message = 'Привіт, Вас вітає  %s!'+ me.first_name + ' Мій id ' + me.id + ' ' + msg.chat.first_name;
+    var message = 'Привіт, Вас вітає '+ myName + ' Мій id ' + myId + 'my Username ' + myUsername + ' ' + msg.chat.first_name;
 
     // отсылаем сообщение, первым параметром передавая id чата,
     // а вторым уже само сообщение
