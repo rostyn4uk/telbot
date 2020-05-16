c45a3029b2ed2b5bcaa43ec41b2cb961954b6cec
rostyn4uk <rostik.manager@gmail.com>
2020-05-16 14:06:41 +0300

    new2 commit12

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const token = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
 // создаём бота
 const bot = new TelegramBot(token, { polling: true });
 
-// проверяем чат и если введена команда "/start", выводим сообщение
 bot.onText(/\/start/, (msg, match) => {
+    var message = '/hello /n /id /n /end /n ' ;
+    bot.sendMessage(msg.chat.id, message);
+});
+
+// проверяем чат и если введена команда "/start", выводим сообщение
+bot.onText(/\/hello/, (msg, match) => {
     // Составляем сообщение, которое будет содержать имя и фамилию того,
     // кто взаимодействует с ботом
     var message = 'Привіт, ' + msg.chat.first_name;
@@ -18,12 +23,7 @@ bot.onText(/\/start/, (msg, match) => {
 });
 
 bot.onText(/\/end/, (msg, match) => {
-    // Составляем сообщение, которое будет содержать имя и фамилию того,
-    // кто взаимодействует с ботом
     var message = 'Бувайте, ' + msg.chat.first_name;
-    //console.log(msg);
-    // отсылаем сообщение, первым параметром передавая id чата,
-    // а вторым уже само сообщение
     bot.sendMessage(msg.chat.id, message);
 });
 
