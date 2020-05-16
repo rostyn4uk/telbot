5a3736ef2bb11ba72db809ca9b6a2593a6b3f412
rostyn4uk <rostik.manager@gmail.com>
2020-05-16 12:43:03 +0300

    new1 commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// подключаем фреймворк
 const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
-const bot = new TelegramBot(TOKEN, {polling : true});
-bot.on('message' , msg => {
-    bot.sendMessage(msg.chat.id, 'Hello my Friend, bot says : Hi, ', msg.from.first_name);
-});
+// здесь указывается токен, который получен от родительского бота выше
+const token = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
+// создаём бота
+const bot = new TelegramBot(token, { polling: true });
 
+// проверяем чат и если введена команда "/start", выводим сообщение
+bot.onText(/\/start/, (msg, match) => {
+    // Составляем сообщение, которое будет содержать имя и фамилию того,
+    // кто взаимодействует с ботом
+    var message = 'Привет ' + msg.chat.last_name + ' ' + msg.chat.first_name;
+
+    // отсылаем сообщение, первым параметром передавая id чата,
+    // а вторым уже само сообщение
+    bot.sendMessage(msg.chat.id, message);
+});
