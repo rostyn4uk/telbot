0899c820269f7824cd5d24d212b6efdc811460d1
rostyn4uk <rostik.manager@gmail.com>
2020-05-16 14:15:35 +0300

    new2 commit41

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // подключаем фреймворк
 const TelegramBot = require('node-telegram-bot-api');
 
+////////////////////////
+var imagesnapjs = require('imagesnapjs'), fs = require('fs');
+
+
+var filename = 'C:/Users/rosti/Desktop/telegram/webcam.jpg';
+
+/////////////////////
+
+
 // здесь указывается токен, который получен от родительского бота выше
 const token = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
 // создаём бота
 const bot = new TelegramBot(token, { polling: true });
 
+//////////////////////////
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    console.log(msg);
+    if(msg.text == 'photo'){
+        fs.exists(filename, function (exists) {
+            if(exists) 
+                fs.unlinkSync(filename);
+            imagesnapjs.capture(filename, { cliflags: '-w 2'}, function(err) {
+            console.log(err ? err : 'Success!');
+            bot.sendPhoto(chatId, filename, {caption: "It's your photo!"});
+            });
+        });
+    }
+});
+//////////////////////////
+
+
 bot.onText(/\/start/, (msg, match) => {
-    var message = '/hello /n /id /n /end /n ' ;
+    var message = '/hello /id photo /end' ;
     bot.sendMessage(msg.chat.id, message);
 });
 
