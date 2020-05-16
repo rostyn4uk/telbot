a6cb103c053bea60fac25a108cdedf9752101d5a
rostyn4uk <rostik.manager@gmail.com>
2020-05-16 13:50:42 +0300

    new2 commit1

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 var TelegramBot = require('node-telegram-bot-api');
-var imagesnapjs = require('imagesnapjs'), fs = require('fs');
-
-var token = 'токен_вашего_бота';
-var filename = '/Users/catethysis/telegram/webcam.jpg';
-
-var bot = new TelegramBot(token, {polling: true});
-
-bot.on('message', function (msg) {
-var chatId = msg.chat.id;
-console.log(msg);
-if(msg.text == 'photo')
-fs.exists(filename, function (exists) {
-if(exists)
-fs.unlinkSync(filename);
-imagesnapjs.capture(filename, { cliflags: '-w 2'}, function(err) {
-console.log(err ? err : 'Success!');
-bot.sendPhoto(chatId, filename, {caption: "It's your photo!"});
+ 
+var token = '938112493:AAGtlVRhc7syYh_ycOo13P7tsNxsoZMlxls';
+var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
 });
+ 
+bot.on('text', function(msg)
+{
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === '/say') {
+        sendMessageByBot(messageChatId, 'Hello World!');
+    }
+ 
+    console.log(msg);
 });
-});
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
